dc7020d02d2ea85c2d3a49b86b6c3bccc1a81832
Samuel Parker <samuel.a.parker00@gmail.com>
2024-10-18 11:54:40 -0400

    add noImplicitAny to typescrip config

--- app/page.tsx
+++ app/page.tsx
@@ -8,26 +8,26 @@ import ChatMessages from "@/components/ChatMessages";
 import ChatAbout from "@/components/ChatAbout";
 
 export default async function Page() {
-	const supabase = supabaseServer();
-	const { data } = await supabase.auth.getSession();
+  const supabase = supabaseServer();
+  const { data } = await supabase.auth.getSession();
 
-	return (
-		<>
-			<div className="max-w-3xl mx-auto md:py-10 h-screen">
-				<div className=" h-full border rounded-md flex flex-col relative">
-					<ChatHeader user={data.session?.user} />
+  return (
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className=" h-full border rounded-md flex flex-col relative">
+          <ChatHeader user={data.session?.user} />
 
-					{data.session?.user ? (
-						<>
-							<ChatMessages />
-							<ChatInput />
-						</>
-					) : (
-						<ChatAbout />
-					)}
-				</div>
-			</div>
-			<InitUser user={data.session?.user} />
-		</>
-	);
+          {data.session?.user ? (
+            <>
+              <ChatMessages />
+              <ChatInput />
+            </>
+          ) : (
+            <ChatAbout />
+          )}
+        </div>
+      </div>
+      <InitUser user={data.session?.user} />
+    </>
+  );
 }
