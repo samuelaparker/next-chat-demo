df0ec88bda88ae94e91b014359ec44d55d44e8a4
Samuel Parker <samuel.a.parker00@gmail.com>
2024-11-05 16:40:50 -0500

    suppress hydration warning see: https://github.com/vercel/next.js/discussions/22388

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={space_Grotesk.className}>
         <ThemeProvider
           attribute="class"
